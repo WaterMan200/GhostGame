a4f62f9912ce471239339cf0723655550e8231ac
siam-cheese <hokiestyle01@gmail.com>
2026-01-12 15:20:43 -0500

    minor map and movement changes

--- Assets/ModularFirstPersonController/FirstPersonController/FirstPersonController.cs
+++ Assets/ModularFirstPersonController/FirstPersonController/FirstPersonController.cs
@@ -446,7 +446,7 @@ public class FirstPersonController : MonoBehaviour
     {
         Vector3 origin = new Vector3(transform.position.x, transform.position.y - (transform.localScale.y * .5f), transform.position.z);
         Vector3 direction = transform.TransformDirection(Vector3.down);
-        float distance = .75f;
+        float distance = .9f;
 
         if (Physics.Raycast(origin, direction, out RaycastHit hit, distance))
         {
