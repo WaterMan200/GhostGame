80838addb80c13786464a207880049deb6888791
siam-cheese <hokiestyle01@gmail.com>
2026-01-21 15:17:55 -0500

    minor changes

--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -9,4 +9,9 @@
           NavMeshAgent agent = GetComponent<NavMeshAgent>();
           agent.destination = goal.position;
        }
+
+       void OnCollisionEnter(Collision other)
+       {
+         if (other.collider.gameObject.tag == "Player") Debug.Log("HAHAHAHAA");
+       }
     }
