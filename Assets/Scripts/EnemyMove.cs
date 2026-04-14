63601a87dd3bee9a1aeda4f66e30e19e79af07d0
WaterMan200 <emanuelguzman0126@gmail.com>
2026-04-14 12:15:05 -0400

    Pausing actually works now

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -18,7 +18,6 @@ public class EnemyMove : MonoBehaviour {
 	private bool following;
 	private bool searching;
 	private bool stalking;
-	private bool frozen;
 	private Vector3 playerLastSeen;
 	private float searchTime;
 	private float speed;
@@ -36,7 +35,6 @@ public class EnemyMove : MonoBehaviour {
 		following = false;
 		searching = false;
 		stalking = false;
-		frozen = false;
 		playerLastSeen = player.transform.position;
 		searchTime = 5f;
 		freezeTime = 0f;
@@ -49,7 +47,7 @@ public class EnemyMove : MonoBehaviour {
 			Targets = enemyVision.visibleTargets;
 			if(Targets.Count > 0)
 			{
-				if(playerVision[1].visibleTargets.Count == 0 && following == false) stalking = true;
+				if(playerVision[0].visibleTargets.Count == 0 && following == false) stalking = true;
 				following = true;
 				playerLastSeen = player.transform.position;
 			}
@@ -104,7 +102,7 @@ public class EnemyMove : MonoBehaviour {
 					agent.isStopped = true;
 					freezeTime += Time.deltaTime;
 
-					if(freezeTime >= playerEnemyDist/5f){
+					if(freezeTime >= playerEnemyDist/10f){
 						agent.isStopped = false;
 						stalking = false;
 						freezeTime = 0f;
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
-
+    public GameObject pauseScreen;
+    public GameObject deathScreen;
     [HideInInspector] public bool time;
     public KeyCode pause = KeyCode.Escape;
     void Start()
     {
         time = true;
+        pauseScreen.SetActive(false);
+        deathScreen.SetActive(false);
     }
     void Update()
     {
@@ -17,14 +20,29 @@ public class Menu : MonoBehaviour
             {
                 time = false;
                 Time.timeScale = 0;
+                pauseScreen.SetActive(true);
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
             }
             
             else if (!time)
             {
                 time = true;
                 Time.timeScale = 1;
+                pauseScreen.SetActive(false);
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
             } 
         } 
+    }
+
+    public void Unpause()
+    {
+        time = true;
+        Time.timeScale = 1;
+        pauseScreen.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
 }
